cfdb82e65e2fa37d4306544dbd8b9d282c2ad5a7
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-08-29 20:27:33 -0400

    completed for now

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
 
 
 // displays the current time in jumbotron
-var currentTime = moment().format("ddd MMM Do, YYYY hh:mm");
+var currentTime = moment().format("ddd MMM Do, YYYY HH:mm");
 console.log(currentTime)
 $('#currentDay').text(currentTime);
 
@@ -32,14 +32,30 @@ for (let i = 0; i < buttonSave.length; i++) {
     // adding an eventListner to each button in the button array
     buttonSave[i].addEventListener("click", () => {
         console.log("tacos")
-       console.log(textArea[i].value)
-    //    saving the value of the text area to local storage
-       localStorage.setItem(i, textArea[i].value)
+        console.log(textArea[i].value)
+        //    saving the value of the text area to local storage
+        localStorage.setItem(i, textArea[i].value)
     })
     console.log(localStorage.getItem(i))
     // redefining the value of the text area that we saved in local storage 
     textArea[i].value = localStorage.getItem(i)
+    console.log(textArea[i])
+}
+
+// targets each time bloc eventually
+for (let i = 0; i < textArea.length; i++) {
+    console.log(textArea[i] && moment().format("H"))
+    if (textArea[i].getAttribute("data-hour") === moment().format("H")) { 
+        textArea[i].setAttribute('class', 'timeData present col-8')
+
+
+    } else if (textArea[i].getAttribute("data-hour") < moment().format("H")) {
+         
+            textArea[i].setAttribute('class', 'timeData past col-8')
+    } 
+    else {textArea[i].setAttribute('class', 'timeData future col-8')}
+
 }
 
 
-
+console.log(moment().format("H"))
